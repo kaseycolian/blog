8f502416f6b45b5d599dc4b867de39d9b570a49d
WillieT1986 <warmad2@hotmail.com>
2018-05-29 22:44:33 -0400

    Created authorRepo within AuthorTest Class

--- src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
+++ src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
@@ -1,12 +1,18 @@
 package com.survivingcodingbootcamp.blog;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 
 public class AuthorTest {
 	
+	@Resource
+	private AuthorRepository authorRepo;
+	
 	@Test
 	public void shouldCreateAuthor() {
 		Author author = new Author();
+		author = authorRepo.save(author);
 	}
 	
 }
