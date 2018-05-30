4ced3bca8c30d6f0f90baf71abf70f63f438b653
WillieT1986 <warmad2@hotmail.com>
2018-05-29 22:47:32 -0400

    interface AuthorRepository now extends CrudRepository

--- src/main/java/com/survivingcodingbootcamp/blog/AuthorRepository.java
+++ src/main/java/com/survivingcodingbootcamp/blog/AuthorRepository.java
@@ -1,5 +1,7 @@
 package com.survivingcodingbootcamp.blog;
 
-public interface AuthorRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long>{
 
 }
