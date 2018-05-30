fe7f422ec015a9cfa148190c79e83cb95e2b411a
WillieT1986 <warmad2@hotmail.com>
2018-05-29 23:22:25 -0400

    Uncommented authorRepo and test fails and points to NullPointerException.

--- src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
+++ src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
@@ -3,6 +3,8 @@ package com.survivingcodingbootcamp.blog;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -10,13 +12,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 public class AuthorTest {
 	
 	
-//	@Resource
-//	private AuthorRepository authorRepo;
+	@Resource
+	private AuthorRepository authorRepo;
 	
 	@Test
 	public void shouldCreateAuthor() {
 		Author author = new Author("Joe");
-//		author = authorRepo.save(author);
+		author = authorRepo.save(author);
 		long authorId = author.getId();
 		
 //		author = author.findOne(authorId);
