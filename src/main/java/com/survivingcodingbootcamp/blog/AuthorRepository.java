0813f3f57192e8ffc4d92e1478f1f0620a126d53
WillieT1986 <warmad2@hotmail.com>
2018-05-30 20:24:23 -0400

    Starting Author Class over

--- src/main/java/com/survivingcodingbootcamp/blog/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.survivingcodingbootcamp.blog;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
--- src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
+++ src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
@@ -1,22 +1,12 @@
 package com.survivingcodingbootcamp.blog;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-import javax.annotation.Resource;
-
 import org.junit.Test;
 
 public class AuthorTest {
-	
-	@Resource
-	private AuthorRepository authorRepo;
-	
+
 	@Test
 	public void shouldCreateAuthor() {
-		Author author = new Author("Joe");
-		
-		assertThat(author.getAuthorName(), is("Joe"));
+
 		}
 	
 }
