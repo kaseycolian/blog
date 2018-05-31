0c6d2a0e9bb362616b76f7df2d643670f6ab34d2
WillieT1986 <warmad2@hotmail.com>
2018-05-30 20:46:51 -0400

    Recreated AuthorTest to recreate Authot Class.

--- src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
+++ src/test/java/com/survivingcodingbootcamp/blog/AuthorTest.java
@@ -1,12 +1,21 @@
 package com.survivingcodingbootcamp.blog;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class AuthorTest {
+	private Author underTest;
 
+	@Before
+	public void setup() {
+		underTest = new Author();
+	}
+	
 	@Test
-	public void shouldCreateAuthor() {
-
-		}
+	public void shouldConstructAuthor() {
+		assertNotNull(underTest);
+	}
 	
 }
