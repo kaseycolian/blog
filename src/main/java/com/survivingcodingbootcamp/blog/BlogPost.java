a761c8d1694e579f812c3431cdf498e395a07d3b
Benjamin Williams <DeanPBR@gmail.com>
2018-06-01 18:59:36 -0400

    Revert to basic class requirements
    Trying to get this project going before adding complexity.

--- src/main/java/com/survivingcodingbootcamp/blog/BlogPost.java
+++ src/main/java/com/survivingcodingbootcamp/blog/BlogPost.java
@@ -2,4 +2,26 @@ package com.survivingcodingbootcamp.blog;
 
 public class BlogPost {
 
+	private String content;
+	private Author author;
+	private String title;
+
+	public BlogPost(String content, Author author, String title) {
+		this.content = content;
+		this.author = author;
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
 }
--- src/test/java/com/survivingcodingbootcamp/blog/BlogPostTest.java
+++ src/test/java/com/survivingcodingbootcamp/blog/BlogPostTest.java
@@ -1,5 +1,33 @@
 package com.survivingcodingbootcamp.blog;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class BlogPostTest {
+	private static final String TITLE = "Test Title";
+	private static final Author AUTHOR = new Author("Joe", "Blob");
+	private static final String CONTENT = "Test Content";
+
+	private void assertTitle(BlogPost blogPost, String title) {
+		assertThat(blogPost.getTitle(), is(title));
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void shouldInstantiate() {
+		BlogPost underTest;
+	}
+
+	BlogPost underTest = new BlogPost(CONTENT, AUTHOR, TITLE);
+
+	@Test
+	public void shouldHaveContentAuthorAndTitle() {
+		assertThat(underTest.getContent(), is(CONTENT));
+		assertThat(underTest.getAuthor(), is(AUTHOR));
+		assertTitle(underTest, TITLE);
+
+	}
 
 }
