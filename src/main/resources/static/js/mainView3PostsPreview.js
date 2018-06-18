69b14d3173d47228d3f062e0b99ba080a710a768
Kasey Colian <kaseycolian@gmail.com>
2018-06-18 09:52:58 -0400

    switched to For Of Loop for titles Event Listeners instead of ES5 for each loop

---
+++ src/main/resources/static/js/mainView3PostsPreview.js
@@ -0,0 +1,24 @@
+// function loadDoc(){
+
+// const xhr = new XMLHttpRequest();
+// xhr.onready = () => {
+
+// 	if (xhr.status === 404){
+// 		const notFoundRes = JSON.parse(xhr.responseText);
+// 		console.log(notFoundRes);
+// 		window.alert("nothing to load!");
+// 	}
+
+// 	if (xhr.status === 200) {
+// 		console.log(loaded);
+// 		const res = JSON.parse(xhr.responseText);
+// 	}
+
+// 	xhr.open('GET', '/blogPost' + blogId, true);
+// 	xhr.send();
+
+// 	}
+// }
+
+
+const getNewPlan = () => fetch("/blogPost/" + blogID).then(res => res.json()).then(resJson => blogID = resJson.id)
--- src/main/resources/static/js/mainViewEventListeners.js
+++ src/main/resources/static/js/mainViewEventListeners.js
@@ -1,14 +1,22 @@
 const setupEventListeners = () =>{
 	const DOMStrings = {
 		title: '.entry__title'
-	}
+	} 
 
 	const titles = document.querySelectorAll(DOMStrings.title);
-
+	//regular For Each Loop
+	/*
 	for (let i = 0; i<titles.length; i++){
 		titles[i].addEventListener('click', (event) => {
 			console.log('clicked');
-		})
+		});
+	}
+	*/
+	//For Of Loop (ES6)
+	for (const cur of titles){
+		cur.addEventListener('click', (event) =>{
+			console.log('clicked with For Of Loop in ES6');
+		});
 	}
 }
 
