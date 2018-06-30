a32d02ed956f5614beab1231edf2362ed5ef2c54
Kasey Colian <kaseycolian@gmail.com>
2018-06-30 11:27:50 -0400

    only last 3 blogs currently displaying

--- src/main/resources/static/js/mainViewCalls.js
+++ src/main/resources/static/js/mainViewCalls.js
@@ -5,58 +5,48 @@ const querySelected = {
 	allTitles: document.querySelectorAll('.title'),
 	blogSection: document.querySelector('section')
 } 
-// const setupEventListeners = () =>{
-
-
-// 	for (const cur of querySelected.allTitles){
-// 		cur.addEventListener('click', (event) =>{
-// 			console.log('clicked with For Of Loop in ES6');
-
-// 			//opening up individal blog in new window
-// 			let blogId = blogJsonData.id;
-// 			window.open(`/blogEntry/${blogId}`);
-// 		});
-// 	}
-// }
-
-// setupEventListeners();
-// const xhr = new XMLHttpRequest();
-
 
 function getBlogs() {
 	fetch(`/blogPosts`)
 	.then(result => {
 		return result.json();
 	})
-	.then(currentBlogs => {
-		const blogPost = currentBlogs._embedded.blogPosts;
-		const reverse = blogPost.reverse();
-	
-		for (const cur of reverse) {
-			const markup = `
+	.then(currentBlogs => {		
+		
+		function postBlogs() {		
+			const blogPosts = currentBlogs._embedded.blogPosts;
 
-				<article class = "blog__entry" id = "entry__one" dataset = "post_1">
-					<div class = "entry__title">
-						<a href = ${cur._links.self.href}><h2>${cur.title}</h2></a>
-					</div>
-					<div class = "entry__info">
-						<h4>${cur.creationDate} ~ ${cur._links.author.href} ~&nbsp</h4>
-						<h4 class = "entry__topic">${cur.topic}</h4>
-					</div>
-					<div class = "entry__info__mobile">
-						<h4>${cur.creationDate}</h4>
-						<h4>AUTHOR GOES HERE</h4>
-						<h4 class = "entry__topic">${cur.topic}</h4>
-					</div>
-					<p class = "entry__content">${cur.content}</p>
-					<div class = article__separator>
-					</div>
-				</article>
-			`
-			querySelected.blogSection.insertAdjacentHTML('beforeend', markup);
-		};
+			const reverseOrderBlogsToDisplay = blogPosts.reverse().slice(0,3);
+				
+			for (blogPost of reverseOrderBlogs) {
+				const markup = `
+					<article class = "blog__entry" id = "entry__one" dataset = "post_1">
+						<div class = "entry__title">
+							<a href = ${blogPost._links.blogPost.href} ><h2>${blogPost.title}</h2></a>
+						</div>
+						<div class = "entry__info">
+							<h4>${blogPost.creationDate} ~ ${blogPost._links.author.href} ~&nbsp</h4>
+							<h4 class = "entry__topic">${blogPost.topic}</h4>
+						</div>
+						<div class = "entry__info__mobile">
+							<h4>${blogPost.creationDate}</h4>
+							<h4>${blogPost._links.author.href}</h4>
+							<h4 class = "entry__topic">${blogPost.topic}</h4>
+						</div>
+						<p class = "entry__content">${blogPost.content}</p>
+						<div class = article__separator>
+						</div>
+					</article>
+				`
+				querySelected.blogSection.insertAdjacentHTML('beforeend', markup);
+			};
+
+		};		
+		postBlogs();	
 	})
-	.catch(error => alert(`The error for this page is:
+	.catch(error => 
+		// window.location ="http://localhost:8080/notfound");
+		alert(`The error for this page is:
 
 ${error}`));
 }
