bcacdbbf7d4a3db4df3f44f62619cb7e821dae36
Kasey Colian <kaseycolian@gmail.com>
2018-07-01 21:17:25 -0400

    page list navigation started

--- src/main/resources/static/js/mainViewCalls.js
+++ src/main/resources/static/js/mainViewCalls.js
@@ -5,7 +5,6 @@ const querySelected = {
 	allTitles: document.querySelectorAll('.title'),
 	blogSection: document.querySelector('section'),
 	header: document.querySelector('.headerH1')
-	// pageButtons: document.querySelector
 } 
 
 function getBlogs() {
@@ -31,17 +30,11 @@ function getBlogs() {
 	})
 }
 
-const renderBlogPagination = (blogPosts, page = 1, postsPerPage = 2) => {
-	console.log(blogPosts);
+const displayIndividualPageNumber = () => {
+	//Display number of pages, up to 5? with "..." at end.  Splice with current page in middle of display.
+	//page 5 display: ... 3 4 5 6 7 ...;
 
-	const start = (page-1) * postsPerPage;
-
-	const end = page * postsPerPage;
-
-	blogPosts.slice(start, end).forEach(renderBlogPosts);
-
-	renderPageButtons(page, blogPosts.length, postsPerPage);
-
+	
 };
 
 const renderBlogPosts = blogPost => {
@@ -70,70 +63,119 @@ const renderBlogPosts = blogPost => {
 		querySelected.blogSection.insertAdjacentHTML('beforeend', markup);
 };
 
+//displays type of button that is passed through and displays page number for next && || previous page
 const createPageButton = (page, type) => `
 	<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
-        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <span>Go To Page ${type === 'prev' ? page - 1 : page + 1}</span>
         <img class="search__icon"
             src="./images/${type === 'prev' ? 'left-' : 'right-'}arrows.svg">
         </img>   
     </button>
 `;
 
-const currentPage = (page) => `
+const createPageNumberList = (page) =>  `
+	<div class = "inline-pageNumbers">
+		<div class = "page__numbers" data-goto=${page}>page: ${page}</div>
+		<div class = "page__numbers" data-goto=${page + 1}>${page + 1}</div>
+		<div class = "page__numbers" data-goto=${page + 2}>${page + 2}</div>
+		<div class = "page__numbers" data-goto=${page + 3}>${page + 3}</div>
+		<div class = "page__numbers" data-goto=${page + 4}>${page + 4}</div>
+	</div>	
+`;
+
+//To Display Page Number List:
+		// Take all page count: this.blogPost
+		// Take current page in renderBlogPagination()
+		//pass into renderBlogPagination
+
+
+
+const currentPageDisplay = (page) => `
 	<div class = current-page>
 		<h4>Current Page: ${page}</h4>
 	</div>
-
-`
-
-const renderPageButtons = (page, numPosts, postsPerPage) => {
-	const pages = Math.ceil(numPosts/postsPerPage);
-	let pageButton;
-
-	if (page === 1 && pages > 1) {
-		pageButton = `
-			${currentPage(page)}</span>
-			${pageButton = createPageButton(page, 'next')}
-		`
-		// pageButton = createPageButton(page, 'pext');
-	} else if (page < pages) {
-		pageButton = `
-			${createPageButton(page, 'next')}
-			${currentPage(page)}
-			${createPageButton(page, 'prev')}
-
-		`;
-	} else if (page === pages && page > 1) {
-		pageButton = `
-			${currentPage(page)}
-			${pageButton = createPageButton(page, 'prev')};
-		`
-	}
-
-	querySelected.blogSection.insertAdjacentHTML('afterbegin', pageButton);
-
-};
+`;
 
 const clearPageResultsBeforeLoadingNewPage = () => {
 	//this will also clear out the buttons until buttons get moved to different div from posts
 	querySelected.blogSection.innerHTML = '';
 	//clearing out buttons for next page buttons:
-
-
 }
+
+//takes in this.blogResult from fetch for all blogPosts returned from fetch
+//has variables to calculate pages, number of blogs per page and total posts
+//calculates posts per page
+//calculates current Page
+const renderBlogPagination = (blogPosts, page = 1, postsPerPage = 2) => {
+	console.log(blogPosts);
+	const start = (page-1) * postsPerPage;
+	const end = page * postsPerPage;
+
+	//each blogPost gets passed in as argument to renderBlogPosts()
+	blogPosts.slice(start, end).forEach(renderBlogPosts);
+
+	renderPageButtons(page, blogPosts.length, postsPerPage);
+	renderPageList(page, blogPosts.length, postsPerPage);
+
+};
+
+
+//calculates pages with passed in arguments
+const renderPageButtons = (page, numPosts, postsPerPage) => {
+	const pages = Math.ceil(numPosts/postsPerPage);
+	let pageButton;
+	// let pageList;
+	if (page === 1 && pages > 1) {
+		pageButton = `
+			${currentPageDisplay(page)}</span>
+			${pageButton = createPageButton(page, 'next')}
+		`
+		// pageButton = createPageButton(page, 'pext');
+	} else if (page < pages) {
+		pageButton = `
+			${currentPageDisplay(page)}
+			${createPageButton(page, 'next')}
+			${createPageButton(page, 'prev')}
+
+		`;
+	} else if (page === pages && page > 1) {
+		pageButton = `
+			${currentPageDisplay(page)}
+			${pageButton = createPageButton(page, 'prev')};
+		`
+	}
+	querySelected.blogSection.insertAdjacentHTML('afterbegin', pageButton);
+};
+
+const renderPageList = (page, numPosts, postsPerPage) => {
+	let pageList;
+	const pages = Math.ceil(numPosts/postsPerPage);
+
+	if (page === 1 && pages > 1) {
+		pageList = ` ${createPageNumberList(page)}
+		`;
+		querySelected.blogSection.insertAdjacentHTML('afterbegin', pageList);
+	}
+	
+};
+
 
 
 querySelected.blogSection.addEventListener('click', e => {
 	const pageButton = e.target.closest('.btn-inline');
-
+	const pageList = e.target.closest('.page__numbers');
 	if (pageButton) {
+		console.log(e);
 		const goToPage = parseInt(pageButton.dataset.goto, 10);
 		clearPageResultsBeforeLoadingNewPage();
 		renderBlogPagination(blogResult, goToPage);
 	}
-
+	if (pageList) {
+		console.log(e);
+		const goToPage = parseInt(pageList.dataset.goto, 10);
+		clearPageResultsBeforeLoadingNewPage();
+		renderBlogPagination(blogResult, goToPage);
+	}
 });
-
-
 
 getBlogs();
